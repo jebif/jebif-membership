699069d038051a342c4510acd376eac378095a05
Loïc Paulevé <loic.pauleve@irccyn.ec-nantes.fr>
2011-03-16 14:17:54 +0100

    admin

--- admin.py
+++ admin.py
@@ -7,7 +7,7 @@ class MembershipInline( admin.TabularInline ) :
 
 class MembershipInfoAdmin( admin.ModelAdmin ) :
 	inlines = [MembershipInline]
-	list_display = ('firstname', 'lastname', 'email', 'user', 'active', 'inscription_date', 'deleted')
+	list_display = ('firstname', 'lastname', 'email', 'user', 'active', 'inscription_date', 'laboratory_city', 'laboratory_cp', 'deleted')
 	list_filter = ('active', 'deleted')
 
 admin.site.register(MembershipInfo, MembershipInfoAdmin)
