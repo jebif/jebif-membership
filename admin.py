cc96be319f9287a2fea76ac44eb6f81395f790bb
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2011-11-22 23:54:52 +0100

    ++ admin

--- admin.py
+++ admin.py
@@ -9,8 +9,14 @@ class MembershipInfoAdmin( admin.ModelAdmin ) :
 	inlines = [MembershipInline]
 	list_display = ('firstname', 'lastname', 'email', 'user', 'active', 'inscription_date', 'laboratory_city', 'laboratory_cp', 'deleted')
 	list_filter = ('active', 'deleted')
+	search_fields = ('firstname', 'lastname', 'email', 'user__username')
 
 admin.site.register(MembershipInfo, MembershipInfoAdmin)
 
 admin.site.register(Membership)
+
+class MembershipInfoEmailChangeAdmin( admin.ModelAdmin ) :
+	list_display = ('date', 'info', 'old_email')
+
+admin.site.register(MembershipInfoEmailChange, MembershipInfoEmailChangeAdmin)
 
